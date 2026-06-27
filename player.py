f516c4f73136b32e32e8de0d9d76abdb4bba562b
Jack Verdin <jackverdin@icloud.com>
2026-06-27 13:24:24 -0400

    player.py: Added Player class and __init__ method, imported Coin

--- player.py
+++ player.py
@@ -4,3 +4,16 @@
     This program holds the player class, and all variables and methods relating to it.
     06/27/2026
 """
+
+from coin import Coin
+
+class Player:
+    """
+        The player class, which holds information about a specific player in the game.
+        Attributes: __name:string, __wallet:int=20, __mycoin:Coin()
+    """
+
+    def __init__(self, name):
+        self.__name = name
+        self.__wallet = 20
+        self.__mycoin = Coin()
