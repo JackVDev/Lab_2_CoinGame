88966d82f466dc86c979953a253d6c7cc01aa714
Jack Verdin <jackverdin@icloud.com>
2026-06-27 14:27:00 -0400

    Lab9_JackVDev-1.py: Planned out main while loop, added loop exit logic

--- Lab9_JackVDev-1.py
+++ Lab9_JackVDev-1.py
@@ -11,6 +11,16 @@ def main():
     print("- Coin Matching Game -")
     player1 = Player("Player 1")
     player2 = Player("Player 2")
+    gameover = False
+    while not(gameover):
+        # Flip coins and print the sides
+        # Check for matching sides, if match p1 wins, else p2 wins
+        # Display coins remaining
+        # Loop exit conditions: user input, either player has 0 coins
+        if player1.get_wallet() < 1 or player2.get_wallet() < 1:
+            gameover = True
+        elif input("Do you want to toss the coins? (y/n): ").lower() != "y":
+            gameover = True
 
 if __name__ == "__main__":
     main()
