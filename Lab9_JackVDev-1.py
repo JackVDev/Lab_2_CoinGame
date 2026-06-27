b2c957d992cc1abaa5e2660643d3ffc25e63c36b
Jack Verdin <jackverdin@icloud.com>
2026-06-27 13:58:35 -0400

    Lab9_JackVDev-1.py: Set up main() function to be used in rest of program, imported Player

--- Lab9_JackVDev-1.py
+++ Lab9_JackVDev-1.py
@@ -4,3 +4,11 @@
     This is the main program for the Lab 9 coding activity.
     06/27/2026
 """
+from player import Player
+
+def main():
+    #Run program
+    print("UNFINISHED")
+
+if __name__ == "__main__":
+    main()
