0477b05814819668bcbde1c5e39eef10499dafdf
Jack Verdin <jackverdin@icloud.com>
2026-06-27 13:49:30 -0400

    player.py: Added all required methods to Player class

--- player.py
+++ player.py
@@ -17,3 +17,21 @@ class Player:
         self.__name = name
         self.__wallet = 20
         self.__mycoin = Coin()
+    
+    def toss_coin(self):
+        self.__mycoin.toss()
+    
+    def get_coin_side(self):
+        return self.__mycoin.get_sideup()
+
+    def win_coin(self):
+        self.__wallet += 1
+
+    def lose_coin(self):
+        self.__wallet -= 1
+
+    def get_wallet(self):
+        return self.__wallet
+    
+    def get_name(self):
+        return self.__name
