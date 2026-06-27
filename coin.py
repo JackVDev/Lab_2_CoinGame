0d85c2ec539a5244cde4d571af5ef3fbb1835628
Jack Verdin <jackverdin@icloud.com>
2026-06-27 11:41:52 -0400

    coin.py: Created Coin class and methods

--- coin.py
+++ coin.py
@@ -4,3 +4,21 @@
     This program holds the coin class, and all variables and methods relating to it.
     06/27/2026
 """
+
+from random import choice
+
+class Coin:
+    """
+        The coin class, holds a private attribute __sideup which can be randomized with toss() and read with get_sideup()
+    """
+
+    def __init__(self):
+        # Initializes the class and randomizes __sideup
+        self.__sideup = choice(["Heads", "Tails"])
+
+    def toss(self):
+        # Randomizes __sideup between "Heads" and "Tails"
+        self.__sideup = choice(["Heads", "Tails"])
+
+    def get_sideup(self):
+        return self.__sideup
